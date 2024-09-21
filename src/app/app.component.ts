eb980f07af443223f7860e6aa3c93e7c29d3408b
Victor Torres <vt96902@gmail.com>
2024-09-20 18:24:13 -0600

    add: mision y vision e info de la empresa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ServiciosComponent,NavbarComponent,FooterComponent],
+  imports: [RouterOutlet, ServiciosComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
